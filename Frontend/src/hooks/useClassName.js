cb6db50e4e91bc2a7d854c185a2c901470347a95
ionut-frisan <ionutfrisan@gmail.com>
2023-03-11 15:11:15 +0200

    useClassName hook

---
+++ Frontend/src/hooks/useClassName.js
@@ -0,0 +1,34 @@
+
+const useClassName = (config) => {
+    let res = '';
+
+    const getClassFromArray = (arr) => {
+        arr.forEach((data) => {
+            if (typeof data === "string") res = `${res} ${data}`;
+            if (typeof data === "object") getClassFromObject(data);
+        })
+    }
+
+    const getClassFromObject = (obj) => {
+        Object.keys(obj).forEach((className) => {
+            if (!!obj[className]) res = `${res} ${className}`;
+        })
+    }
+
+    const getClasses = (config) => {
+        config.forEach(data => {
+            if (typeof data === "string") res = `${res} ${data}`;
+            if (typeof data === 'object' && Array.isArray(data)) {
+                getClassFromArray(data);
+                return;
+            }
+            if(typeof data === "object") getClassFromObject(data);
+        })
+    }
+
+    getClasses(config);
+
+    return res;
+}
+
+export default useClassName;

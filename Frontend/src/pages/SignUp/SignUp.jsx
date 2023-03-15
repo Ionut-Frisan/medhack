ecfd51924a949c5b9e44f283f937722af737e409
ionut-frisan <ionutfrisan@gmail.com>
2023-03-15 02:06:00 +0200

    update navbar and signup style

--- Frontend/src/pages/SignUp/SignUp.jsx
+++ Frontend/src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,6 @@
 import {useState} from "react";
 
-import '../../assets/style/pages/signup.scss';
+import './SignUp.scss';
 import MedButton from "../../components/button/MedButton.jsx";
 import MedInput from "../../components/input/MedInput.jsx";
 import Link from "../../components/router/Link.jsx";
@@ -8,40 +8,59 @@ import Link from "../../components/router/Link.jsx";
 import image from "../../assets/images/10132.jpg";
 
 const SignUp = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // TODO: validate values
+        console.warn(firstName);
+    }
+
     return (
         <div className={'flex-2-1'}>
             <div className={'presentation'}>
                 <img src={image}/>
             </div>
-            <div className={'form'}>
-                <h2>App name</h2>
+            <form className={'form'} onSubmit={handleSubmit}>
+                <h1>App name</h1>
                 <MedInput labelPosition={'float'}
                           label={'First name'}
                           size={'large'}
+                          onChange={(e) => setFirstName(e.target.value)}
                 />
                 <MedInput labelPosition={'float'}
                           label={'Last name'}
                           size={'large'}
+                          onChange={(e) => setLastName(e.target.value)}
                 />
                 <MedInput labelPosition={'float'}
                           label={'Email'}
                           type={'email'}
                           size={'large'}
+                          onChange={(e) => setEmail(e.target.value)}
                 />
                 <MedInput labelPosition={'float'}
                           label={'Phone number'}
                           type={'tel'}
                           size={'large'}
+                          onChange={(e) => setPhoneNumber(e.target.value)}
                 />
                 <MedInput labelPosition={'float'}
                           label={'Password'}
                           type={'password'}
                           size={'large'}
+                          onChange={(e) => setPassword(e.target.value)}
                 />
                 <MedInput labelPosition={'float'}
                           label={'Confirm password'}
                           type={'password'}
                           size={'large'}
+                          onChange={(e) => setConfirmPassword(e.target.value)}
                 />
                 <span className={'log-in-option'}>
                     Already have an account?
@@ -53,7 +72,7 @@ const SignUp = () => {
                            label={'Sign up'}
                            size={'large'}>
                 </MedButton>
-            </div>
+            </form>
         </div>
     );
 }

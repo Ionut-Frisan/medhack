a105da269e53d872d610e90092f346dba7dfaf26
Denisa Dunca <duncadenisa05@gmail.com>
2023-03-17 22:05:19 +0200

    fish fosh

--- Frontend/src/pages/Welcome/Welcome.jsx
+++ Frontend/src/pages/Welcome/Welcome.jsx
@@ -1,26 +1,25 @@
 import "./Welcome.scss"
-import image from "../../assets/images/ilustratie_-_informare_despre_vaccin.svg"
+import MedButton from "../../components/button/MedButton"
 
 function Welcome() {
     return (
         <div className={"welcome-page"}>
             <div className={"welcome-text"}>
                 <div className={"welcome-title"}>
-                    <h1 className={"font-link"}>
+                    <h1 className={"font-link-title"}>
                         Bine ați venit pe site-ul nostru de
-                        <h2> Vaccinare pentru copii!</h2>
+                        <h2 className={"welcome-title-accent"}> Vaccinare pentru copii!</h2>
                     </h1>
                 </div>
                 <div className={"description"}>
-                    <h1 className={"font-link"}>
+                    <h1 className={"font-link-description"}>
                         Ca părinți, știm cât de important este să ne menținem copiii sănătoși și în siguranță.
                         Un mod prin care putem face acest lucru este asigurându-ne că aceștia primesc vaccinurile
                         recomandate.
                     </h1>
                 </div>
+                <MedButton label={"Informează-te"} />
             </div>
-
-            <img src={image} alt={"welcome-illustration"}/>
         </div>
 
     )

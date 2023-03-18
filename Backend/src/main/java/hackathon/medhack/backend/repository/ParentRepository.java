2419877d443b7265d995564470f01192b572d6e0
DaridaRazvan <razvandarida.rd@gmail.com>
2023-03-18 11:10:17 +0200

    search parent

--- Backend/src/main/java/hackathon/medhack/backend/repository/ParentRepository.java
+++ Backend/src/main/java/hackathon/medhack/backend/repository/ParentRepository.java
@@ -3,10 +3,13 @@ package hackathon.medhack.backend.repository;
 import hackathon.medhack.backend.model.Parent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ParentRepository extends JpaRepository<Parent, Long> {
     Optional<Parent> getByEmailAndPassword(String email, String password);
 
     Optional<Parent> getByEmail(String email);
+
+    List<Parent> getByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(String firstName, String lastName);
 }

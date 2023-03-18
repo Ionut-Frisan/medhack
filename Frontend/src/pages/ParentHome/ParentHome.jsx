6deda0c2743634f4d0aa4a9b54866ab3cd32741d
Ionut-Frisan <ionutfrisan@gmail.com>
2023-03-18 16:35:48 +0200

    initial parent homepage

--- Frontend/src/pages/ParentHome/ParentHome.jsx
+++ Frontend/src/pages/ParentHome/ParentHome.jsx
@@ -2,18 +2,28 @@ import useRequest from "../../hooks/useRequest.js";
 import {useSelector} from "react-redux";
 import {getToken} from "../../store/featutres/auth/auth-slice.js";
 import {useEffect, useState} from "react";
+import ChildCard from "./ChildCard.jsx";
 
 const ParentHome = () => {
     const [children, setChildren] = useState([]);
     const {get} = useRequest();
     const parentId = useSelector(getToken);
-    const getChildren = () => { get(
-        `api/parent/children/${parentId}`,
-        {},
-        ({data}) => setChildren(data || [])
-    )};
+    const getChildren = () => {
+        if(!!parentId){
+            get(
+            `api/parent/children/${parentId}`,
+            {},
+            ({data}) => setChildren(data || [])
+            )
+        }
+    };
     useEffect(() => getChildren(), [parentId]);
-    return (<div>ParentHome page</div>)
+    
+    const childrenList = children.map((child) => {
+        return <ChildCard child={child}></ChildCard>
+    })
+    
+    return (<div className={'child-list-main'}>{childrenList}</div>)
 };
 
 export default ParentHome;

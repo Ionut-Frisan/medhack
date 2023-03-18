1a427894eb21684f2f09a02a3db37f19c2900d03
DaridaRazvan <razvandarida.rd@gmail.com>
2023-03-19 00:18:18 +0200

    made pics smaller size, change routing

--- Frontend/src/components/NavBar/NavBar.jsx
+++ Frontend/src/components/NavBar/NavBar.jsx
@@ -35,7 +35,7 @@ const NavBar = ({}) => {
             <FaMedrt/>
             <ul className={'navbar-links'}>
                 <li>
-                    <Link to="/vaccines">Vaccine list</Link>
+                    <Link to="/vaccineDictionary">Vaccine list</Link>
                 </li>
                 <li>
                     <Link to="/faq">FAQ</Link>

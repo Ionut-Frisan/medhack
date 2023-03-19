5abd94c2e05a5b27f5fd0dc0dd39f2b460e504f8
LidiaLuncian <lidia.luncian15@gmail.com>
2023-03-19 14:44:45 +0200

    send email

--- Backend/src/main/java/hackathon/medhack/backend/service/implementation/ParentServiceImplementation.java
+++ Backend/src/main/java/hackathon/medhack/backend/service/implementation/ParentServiceImplementation.java
@@ -1,22 +1,23 @@
 package hackathon.medhack.backend.service.implementation;
 
 import hackathon.medhack.backend.exceptions.CustomException;
-import hackathon.medhack.backend.model.Child;
-import hackathon.medhack.backend.model.Doctor;
-import hackathon.medhack.backend.model.Parent;
+import hackathon.medhack.backend.model.*;
 import hackathon.medhack.backend.model.dto.ChildDto;
+import hackathon.medhack.backend.model.dto.ChildVaccineDto;
+import hackathon.medhack.backend.model.dto.EmailDto;
 import hackathon.medhack.backend.model.dto.ParentDto;
 import hackathon.medhack.backend.model.mapper.ChildMapper;
 import hackathon.medhack.backend.model.mapper.ParentMapper;
-import hackathon.medhack.backend.repository.DoctorRepository;
-import hackathon.medhack.backend.repository.ParentRepository;
+import hackathon.medhack.backend.repository.*;
 import hackathon.medhack.backend.service.ParentService;
+import hackathon.medhack.backend.utils.SendEmail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -31,6 +32,11 @@ public class ParentServiceImplementation implements ParentService {
     private final ParentMapper parentMapper;
 
     private final ChildMapper childMapper;
+
+    private final ChildVaccineRepository childVaccineRepository;
+    private final VaccineRepository vaccineRepository;
+
+    private final ChildRepository childRepository;
 
     @Override
     public List<ParentDto> getAllParents() {
@@ -80,7 +86,7 @@ public class ParentServiceImplementation implements ParentService {
         Parent parent = parentMapper.convertParentDtoToParent(parentDto);
         parent.setDoctor(doctor);
         parentRepository.save(parent);
-
+        SendEmail.sendConfirmationSignUpMail(parent);
         return parent.getId();
     }
 
@@ -96,5 +102,27 @@ public class ParentServiceImplementation implements ParentService {
         parents.forEach(parent -> parentsDto.add(parentMapper.convertParentToParentDto(parent)));
 
         return parentsDto;
+    }
+
+    @Override
+    public String sendMemoForParent(EmailDto emailDto){
+        Doctor doctor = doctorRepository.findById(emailDto.getDoctorId()).get();
+        String doctorEmail = doctor.getEmail();
+        Parent parent = parentRepository.findById(emailDto.getParentId()).get();
+        String parentEmail = parent.getEmail();
+        ArrayList<ChildVaccineDto> childVaccines = (ArrayList<ChildVaccineDto>) childVaccineRepository.getChildVaccines(emailDto.getChildId());
+        Long vaccineId = -1L;
+        for(ChildVaccineDto childVaccine : childVaccines){
+            if(!childVaccine.getIsDone()){
+                vaccineId = childVaccine.getId();
+                break;
+            }
+        }
+        Child child = childRepository.findById(emailDto.getChildId()).get();
+        if(vaccineId >=0 ){
+            Vaccine vaccine = vaccineRepository.findById(vaccineId).get();
+            SendEmail.sendVaccineMemo(doctorEmail, parentEmail, child, vaccine);
+        }
+        return "Email sent";
     }
 }

f82abee7b331a47d3b926138158c2b3301ea9a23
DaridaRazvan <razvandarida.rd@gmail.com>
2023-03-19 14:25:56 +0200

    client message from doctor css

--- Frontend/src/pages/Notifications/Message.jsx
+++ Frontend/src/pages/Notifications/Message.jsx
@@ -1,10 +1,21 @@
 import "./Message.scss";
-
+import UserImage from "../../components/user-image/UserImage.jsx";
+import doctor from "../../assets/images/doctor-img.png";
 const Message =(props) => {
+    const date = new Date(props.date).toLocaleString();
     return(
-        <div className={"message"}>
-            {props.msg}
-            {props.date}
+        <div className={"messageContainer"}>
+            <div>
+                <UserImage src={doctor} alt={"Doctor"}/>
+            </div>
+            <div className={"message"}>
+                    <div className={"messageDate"}>
+                        {date}
+                    </div>
+                    <div className={"messageText"}>
+                        {props.msg}
+                    </div>
+            </div>
         </div>
     )
 }

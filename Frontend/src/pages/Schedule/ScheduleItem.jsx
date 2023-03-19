82bdca9e0c0bed8a265c1bffbdbcbf43c45b29f6
DaridaRazvan <razvandarida.rd@gmail.com>
2023-03-19 11:48:18 +0200

    send msg

--- Frontend/src/pages/Schedule/ScheduleItem.jsx
+++ Frontend/src/pages/Schedule/ScheduleItem.jsx
@@ -1,18 +1,43 @@
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import useRequest from "../../hooks/useRequest.js";
+import {useSelector} from "react-redux";
+import {getToken} from "../../store/featutres/auth/auth-slice.js";
 
 const ScheduleItem = (props) => {
+    const [sendMessage, setSendMessage] = useState(false);
     const [name, setName] = useState();
-    const {get} = useRequest();
+    const [parentId, setParentId] = useState();
+    const {get, post} = useRequest();
+    const doctorId = useSelector(getToken);
+
+    const textBoxText = useRef();
 
     useEffect(() => {
         const getChildName = async () => {
             const res = await get(`api/child_vaccine/getChild/${props.childVaccineId}`);
             const tmpName = res.data.firstName + res.data.lastName;
             setName(tmpName);
+            setParentId(res.data.parentId);
         }
         getChildName().then();
     }, [])
+
+    const sendMessageToParent = () => {
+        console.log(textBoxText.current.value);
+        console.log("Sending message to " + parentId);
+
+        const sendMsg = async () => {
+            const body = {
+                doctorId,
+                parentId,
+                message:textBoxText.current.value
+            }
+            const res = await post('api/message/add', {data: body});
+            console.log(res.data);
+        }
+
+        sendMsg().then();
+    }
 
     return (
       <div>
@@ -23,7 +48,13 @@ const ScheduleItem = (props) => {
               {props.childVaccineDate}
               {name}
           </div>
-          <button>Send Message</button>
+          <button onClick={() => setSendMessage(!sendMessage)}>Contact Parent</button>
+          {sendMessage &&
+              <div>
+                  <textarea ref={textBoxText}></textarea>
+                  <button onClick={sendMessageToParent}>Send</button>
+              </div>
+          }
       </div>
     );
 }

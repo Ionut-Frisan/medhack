022452aec7f0a169be4a422e7643e82e4f6f711b
DaridaRazvan <razvandarida.rd@gmail.com>
2023-03-19 16:35:05 +0200

    notifications toasty

--- Frontend/src/pages/ParentHome/ParentHome.jsx
+++ Frontend/src/pages/ParentHome/ParentHome.jsx
@@ -3,11 +3,14 @@ import {useSelector} from "react-redux";
 import {getToken} from "../../store/featutres/auth/auth-slice.js";
 import {useEffect, useState} from "react";
 import ChildCard from "./ChildCard.jsx";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ParentHome = () => {
     const [children, setChildren] = useState([]);
     const {get} = useRequest();
     const parentId = useSelector(getToken);
+
     const getChildren = () => {
         if(!!parentId){
             get(
@@ -18,12 +21,33 @@ const ParentHome = () => {
         }
     };
     useEffect(() => getChildren(), [parentId]);
+
+    useEffect(() => {
+        const popNotification = async () => {
+            const res = await get(`api/parent/getNextVaccine/${parentId}`)
+            return res.data;
+        }
+        if(parentId){
+            popNotification().then(data => {
+                const message = `Aveti o programare pentru ${data.abbreviation || data.name} in data de ${data.childVaccineDate}.`;
+                toast(message);
+            });
+        }
+
+    },[parentId]);
     
     const childrenList = children.map((child) => {
         return <ChildCard child={child}></ChildCard>
     })
     
-    return (<div className={'child-list-main'}>{childrenList}</div>)
+    return (
+        <div>
+            <ToastContainer />
+            <div className={'child-list-main'}>
+                {childrenList}
+            </div>)
+        </div>
+    )
 };
 
 export default ParentHome;
